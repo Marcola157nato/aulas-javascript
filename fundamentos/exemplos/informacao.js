7c69d2d90b25b512d1262efe00b44953713fcf37
Marcola <marcus.h.cavalcante@aluno.senai.br>
2026-02-06 09:22:22 -0300

    arquivo informacao

---
+++ fundamentos/exemplos/informacao.js
@@ -0,0 +1,12 @@
+const curso = "tecnico de computaria"
+const ano = "2k25"
+const NotaMinimaParaSerAprovado = 50
+const NotaParaSerReprovado = 49.9
+const CursoTecnico = true
+
+console.log("Minhas informações academicas")
+console.log("Curso: "+ curso)
+console.log(`ano: ${ano}`)
+console.log(`Nota para Aprovação ${NotaMinimaParaSerAprovado}`)
+console.log(`Nota minima para ser reprovado ${NotaParaSerReprovado}`)
+console.log(`É curso tecnico: ${CursoTecnico}`)
